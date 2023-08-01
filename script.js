4cd45fe9c4bf55ea8d1c2dab81b5d8626b4f4c47
Rakesh Patel <rakesh246808@gmail.com>
2023-08-01 10:48:51 +0530

    adding extra features

--- script.js
+++ script.js
@@ -1,13 +1,48 @@
-document.querySelectorAll('input[type=color]').forEach(function (picker) {
-    var targetLabel = document.querySelector('label[for="' + picker.id + '"]'),
-        colorArea = document.getElementById('ColorCode');
 
-    colorArea.innerHTML = picker.value;
-    // targetLabel.appendChild(colorArea);
-
-    picker.addEventListener('change', function() {
-        colorArea.innerHTML = picker.value;
-        
-        // targetLabel.appendChild(colorArea);
-    });
+let colorIndicator = document.getElementById('color-indicator');
+let colorValue = document.getElementById('ColorCode');
+const colorPicker = new iro.ColorPicker("#color-picker", {
+    width:190, 
+    color: "#ffffff"
+    
 });
+var hex = colorPicker.color.hexString;
+// console.log(hex); // hex = "#ff0000"
+
+colorPicker.on('color:change', function(color) {
+    colorIndicator.style.backgroundColor = color.hexString;
+    colorIndicator.style.boxshadow = color.hexString;
+});
+
+
+colorPicker.on(['color:init', 'color:change'], function(color) {
+// log the current color as a HEX string
+colorValue.innerHTML = color.hexString;
+colorIndicator.style.backgroundColor = colorValue;
+console.log(color.hexString);                 // you cane get hex value from here
+});
+
+
+//*******************    #to do     **************************
+function myFunction() {
+    colorValue.innerHTML = color.hexString;
+    document.getElementById("ColorCode").style.boxShadow = `10px 20px 30px ${colorValue}`;
+
+  }
+
+function toggle()
+{
+    let p = document.getElementById("hexvalue");
+    let c = document.getElementById("color");
+    if(p.style.display === "none")
+    {
+        p.style.display = "block";
+        c.style.display = "flex";
+        c.style.justifyContent = "center";
+        c.style.alignItems = "center";
+    }
+    else
+    {
+        p.style.display = "none";
+    }
+}  
