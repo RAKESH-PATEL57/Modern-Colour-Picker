8e07bd7acd3ecf3e99614cb444b537f693359981
Rakesh Patel <rakesh246808@gmail.com>
2023-08-02 10:16:23 +0530

    switching

--- script.js
+++ script.js
@@ -1,48 +1,93 @@
 
-let colorIndicator = document.getElementById('color-indicator');
-let colorValue = document.getElementById('ColorCode');
+let colorIndicator = document.getElementById("color-indicator");
+let colorValue = document.getElementById("ColorCode");
 const colorPicker = new iro.ColorPicker("#color-picker", {
     width:190, 
     color: "#ffffff"
     
 });
-var hex = colorPicker.color.hexString;
-// console.log(hex); // hex = "#ff0000"
-
-colorPicker.on('color:change', function(color) {
-    colorIndicator.style.backgroundColor = color.hexString;
-    colorIndicator.style.boxshadow = color.hexString;
-});
 
 
-colorPicker.on(['color:init', 'color:change'], function(color) {
-// log the current color as a HEX string
-colorValue.innerHTML = color.hexString;
-colorIndicator.style.backgroundColor = colorValue;
-console.log(color.hexString);                 // you cane get hex value from here
-});
-
-
-//*******************    #to do     **************************
-function myFunction() {
-    colorValue.innerHTML = color.hexString;
-    document.getElementById("ColorCode").style.boxShadow = `10px 20px 30px ${colorValue}`;
-
-  }
-
-function toggle()
+colorPicker.on(['color:init', 'color:change'], function(color) 
 {
-    let p = document.getElementById("hexvalue");
-    let c = document.getElementById("color");
-    if(p.style.display === "none")
+    colorValue.innerHTML = color.hexString;
+    colorIndicator.style.backgroundColor = color.hexString;
+    colorIndicator.style.boxShadow = `0px 0px 20px ${color.hexString}`;
+    // console.log(color.hexString);                 // you can get hex value from here
+});
+
+
+
+
+  let p = document.getElementById("hexvalue");
+  let c = document.getElementById("choose");
+  let s = document.getElementById("hexvalue2");
+  let t = document.getElementById("hexvalue3");
+
+function hexvalue()
+{
+    do
     {
         p.style.display = "block";
         c.style.display = "flex";
         c.style.justifyContent = "center";
         c.style.alignItems = "center";
+        s.style.display = "none";
+        t.style.display = "none";
     }
-    else
+    while(p.style.display === "none")
     {
-        p.style.display = "none";
-    }
+        p.style.display = "block";
+        c.style.display = "flex";
+        c.style.justifyContent = "center";
+        c.style.alignItems = "center";
+        s.style.display = "none";
+        t.style.display = "none";
+    };
+
 }  
+
+function rgb()
+{
+    do{
+        s.style.display = "block";
+        c.style.display = "flex";
+        c.style.justifyContent = "center";
+        c.style.alignItems = "center";
+        p.style.display = "none";
+        t.style.display = "none";
+    }
+    while(s.style.display === "none")
+    {
+        s.style.display = "block";
+        c.style.display = "flex";
+        c.style.justifyContent = "center";
+        c.style.alignItems = "center";
+        p.style.display = "none";
+        t.style.display = "none";
+    };
+}
+
+function gradient()
+{
+    do
+    {
+        
+        t.style.display = "block";
+        c.style.display = "flex";
+        c.style.justifyContent = "center";
+        c.style.alignItems = "center";
+        p.style.display = "none";
+        s.style.display = "none";
+    }
+    while(t.style.display === "none")
+    {
+        t.style.display = "block";
+        c.style.display = "flex";
+        c.style.justifyContent = "center";
+        c.style.alignItems = "center";
+        p.style.display = "none";
+        s.style.display = "none";
+    };
+
+}
